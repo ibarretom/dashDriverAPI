8bbc09d1b4bdac3d4468b1275836c7815a501b66
ibarretom <igorbm.23@gmail.com>
2023-03-10 03:15:06 -0300

    Migration de usuários

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,14 +1,17 @@
+import 'reflect-metadata'
 import { DataSource } from 'typeorm'
+
+import { createUser1678407280639 } from '../migrations/1678407280639-create_user'
 
 const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DATABASE_HOST,
   port: 5432,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  username: 'root',
+  password: 'password',
+  database: 'dash_driver',
   entities: [],
-  migrations: [],
+  migrations: [createUser1678407280639],
   synchronize: true,
   logging: false,
 })
---
+++ src/migrations/1678407280639-create_user.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createUser1678407280639 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+            length: '255',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'photo_url',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
