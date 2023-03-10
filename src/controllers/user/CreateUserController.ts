114e651f3b88092f15c9ea1a7b8d89f38686eed8
ibarretom <igorbm.23@gmail.com>
2023-03-10 04:20:19 -0300

    Validação do request com zod

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
 
 export class CreateUserController {
   handle(request: Request, response: Response): Response {
-    const { name, email, password } = request.body
+    const requestSchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+      password: z.string().min(8),
+    })
 
-    return response.status(201).json({ name, email, password })
+    try {
+      const { name, email, password } = requestSchema.parse(request.body)
+
+      return response.status(201).json({ name, email, password })
+    } catch (err) {
+      return response.status(400).json(err)
+    }
   }
 }
