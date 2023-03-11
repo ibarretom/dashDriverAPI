797cd4442331ea11d847356dba623cb15d3c4ce3
ibarretom <igorbm.23@gmail.com>
2023-03-10 22:26:02 -0300

    Hash de password

--- src/service/users/CreateUsers.service.ts
+++ src/service/users/CreateUsers.service.ts
@@ -1,6 +1,6 @@
 import { User } from '../../entities/User'
 import { IUsersRepository } from '../../repositories/user/IUsersRepository'
-
+import { hash } from 'bcrypt'
 type UserRequest = {
   name: string
   email: string
@@ -15,8 +15,14 @@ export class CreateUsersService {
   }
 
   async execute(user: UserRequest): Promise<User> {
+    const hashed_password = await this.hashPassword(user.password)
+
     try {
-      const createdUser = await this.usersRepository.create(user)
+      const createdUser = await this.usersRepository.create({
+        ...user,
+        password: hashed_password,
+      })
+
       return createdUser
     } catch (err: any) {
       if (err.code == '23505') {
@@ -25,5 +31,14 @@ export class CreateUsersService {
 
       throw new Error('Error creating user')
     }
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    const hashedPassword = await hash(
+      password,
+      Number(process.env.PASSWORD_SALT)
+    )
+
+    return hashedPassword
   }
 }
