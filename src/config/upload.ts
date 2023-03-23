554a24839baccc2a5c2915b9d4f1578421d4db53
ibarretom <igorbm.23@gmail.com>
2023-03-23 01:42:59 -0300

    Configuração de upload e provider de storage

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import multer from 'multer'
+import { resolve } from 'path'
+import { randomBytes } from 'crypto'
+
+const tmpFolder = resolve(__dirname, '..', '..', 'tmp')
+
+export default {
+  tmpFolder,
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (req, file, cb) => {
+      const fileHash = randomBytes(16).toString('hex')
+      const filename = `${fileHash}-${file.originalname}`
+
+      return cb(null, filename)
+    },
+  }),
+}
